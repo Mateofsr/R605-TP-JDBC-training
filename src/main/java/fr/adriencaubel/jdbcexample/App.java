7e3a356f0fe6e3e05866a848ff0eed6946ff48ea
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2025-01-29 14:23:37 +0100

    chore:END

--- src/main/java/fr/adriencaubel/jdbcexample/App.java
+++ src/main/java/fr/adriencaubel/jdbcexample/App.java
@@ -1,5 +1,10 @@
 package fr.adriencaubel.jdbcexample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class App {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3312/jdbc-training";
@@ -9,8 +14,15 @@ public class App {
         try {
             // Load MySQL JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn =
+                    DriverManager
+                            .getConnection(url, user, password);
 
-            
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS client (id INT AUTO_INCREMENT PRIMARY KEY,nom VARCHAR(100) DEFAULT 'Marcel', age INT DEFAULT 0)");
+
+            stmt.executeUpdate("INSERT INTO client (nom, age) VALUES ('Bob', 25)");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
